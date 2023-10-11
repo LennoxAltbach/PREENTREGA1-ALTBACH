739e5033d983cc09d33535fd0672ec624d284958
LennoxAltbach <altlennox@gmail.com>
2023-10-11 14:40:26 -0300

    objetos y arrays

--- js/script.js
+++ js/script.js
@@ -1,39 +1,62 @@
+const monedas = [
+  { nombre: 'Dólares', tasaCambio: 1000 },
+  { nombre: 'Euros', tasaCambio: 1100 },
+  { nombre: 'Reales', tasaCambio: 120 }
+];
+
+const historialConversiones = [];
+
+function mostrarOpcionesMonedas() {
+  console.log('Opciones de monedas:');
+  monedas.forEach((moneda, index) => {
+    console.log(`${index + 1}. Convertir a ${moneda.nombre}`);
+  });
+}
+
+function calcularResultado(cantidad, tasaCambio) {
+  return cantidad / tasaCambio;
+}
+
+function mostrarHistorial() {
+  console.log('Historial de conversiones:');
+  historialConversiones.forEach((conversion, index) => {
+    console.log(`${index + 1}. Convertido a ${conversion.moneda}: ${conversion.cantidad} pesos = ${conversion.resultado} ${conversion.moneda}`);
+  });
+}
+
 function convertirMoneda() {
-    alert("Bienvenido al conversor de moneda, cambia tus pesos a la moneda que elijas");
-  
-    const cantidadDinero = parseFloat(prompt("Por favor, ingresa la cantidad de dinero:"));
-    if (isNaN(cantidadDinero)) {
-      alert("Debe ingresar un número válido. Recarga la página e intenta nuevamente.");
-      return;
-    }
-  
-    const opcion = parseInt(prompt("Elige una opción:\n1. Convertir a dólares $\n2. Convertir a euros €\n3. Convertir a reales R  "));
-  
-    if (opcion >= 1 && opcion <= 3) {
-      let resultado;
-  
-      switch (opcion) {
-        case 1:
-          resultado = cantidadDinero / 1025;
-          break;
-        case 2:
-          resultado = cantidadDinero / 1114; 
-          break;
-        case 3:
-          resultado = cantidadDinero / 100;
-          break;
-      }
-  
-      const resultadoRedondeado = Math.round(resultado);
-  
-      let mensajeRedondeo = resultadoRedondeado > resultado ? "se redondeó hacia arriba" : "se redondeó hacia abajo";
-  
-      alert(`El monto en la moneda seleccionada es: ${resultadoRedondeado}. (${mensajeRedondeo})`);
-      alert("Gracias por usar nuestro conversor de moneda");
-    } else {
-      alert("Opción incorrecta. vuelve a tocar el boton.");
-    }
+  alert("Bienvenido al conversor de moneda, cambia tus pesos a la moneda que elijas");
+
+  const cantidadDinero = parseFloat(prompt("Por favor, ingresa la cantidad de dinero:"));
+  if (isNaN(cantidadDinero)) {
+    alert("Debe ingresar un número válido. Recarga la página e intenta nuevamente.");
+    return;
   }
-  
-  let button = document.getElementById("conversor");
-  button.onclick = convertirMoneda;
+
+  mostrarOpcionesMonedas();
+
+  const opcion = parseInt(prompt("Elige una opción:\n1. Convertir a dólares $\n2. Convertir a euros €\n3. Convertir a reales R  "));
+  if (opcion >= 1 && opcion <= 3) {
+    const monedaSeleccionada = monedas[opcion - 1];
+    const resultado = cantidadDinero / monedaSeleccionada.tasaCambio;
+
+    const resultadoRedondeado = Math.round(resultado);
+    let mensajeRedondeo = resultadoRedondeado > resultado ? "se redondeó hacia arriba" : "se redondeó hacia abajo";
+
+    alert(`El monto en ${monedaSeleccionada.nombre} es: ${resultadoRedondeado}. (${mensajeRedondeo})`);
+
+    historialConversiones.push({
+      moneda: monedaSeleccionada.nombre,
+      cantidad: cantidadDinero,
+      resultado: resultadoRedondeado
+    });
+
+    mostrarHistorial();
+    alert("Gracias por usar nuestro conversor de moneda");
+  } else {
+    alert("Opción incorrecta. Vuelve a tocar el botón.");
+  }
+}
+
+let button = document.getElementById("conversor");
+button.onclick = convertirMoneda;
